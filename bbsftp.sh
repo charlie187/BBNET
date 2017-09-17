f913211852ede48cb6164cfdedac175aea87d85d
charlie187 <32032426+charlie187@users.noreply.github.com>
2017-09-17 12:07:36 +0200

    bbSFTP

---
+++ bbsftp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curip=$(ifconfig | grep 'inet 192' | awk '{print $2}')
+echo 'SFTPConnect to BBNET v1.2'
+echo ''
+if [[ $curip != '192.168.0.200' ]]; then
+  echo ''
+  echo 'Logging into BBNET'
+  sftp -P 4447 -i /home/charlie187/.ssh/bbnet.key 82.174.98.210
+  exit
+else
+  sftp -P 4447 -i /home/charlie187/.ssh/bbnet.key 192.168.1.203
+fi
